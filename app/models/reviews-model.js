79f7dfa7247719e2b9d2d5fcc748f822640f8790
GWorsnop <georgeworsnop@gmail.com>
2022-07-04 17:25:51 +0100

    Include correct SQL query

--- app/models/reviews-model.js
+++ app/models/reviews-model.js
@@ -1,31 +1,27 @@
 const connection = require("../../db/connection");
 
 exports.selectReviewById = (review_id) => {
-  return (
-    connection
-      .query(
-        `
-          SELECT *
-          FROM reviews
-          WHERE reviews.review_id = $1
+  return connection
+    .query(
+      `
+        SELECT reviews.*, COUNT(comments.body)::int AS comment_count FROM reviews
+        LEFT JOIN comments ON reviews.review_id = comments.review_id
+        WHERE reviews.review_id = $1
+        GROUP BY reviews.review_id;
           `,
-        [review_id]
-      )
-      // COUNT(reviews.review_id)
-      // RIGHT JOIN comments ON reviews.review_id = comments.review_id
-      // GROUP BY reviews.review_id
+      [review_id]
+    )
 
-      .then((result) => {
-        if (result.rows.length > 0) {
-          return result.rows[0];
-        } else {
-          return Promise.reject({
-            status: 404,
-            errorMessage: "Bad Request - review_id does not exist",
-          });
-        }
-      })
-  );
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return Promise.reject({
+          status: 404,
+          errorMessage: "Bad Request - review_id does not exist",
+        });
+      }
+    });
 };
 
 exports.updateReviewVotes = (inc_votes, review_id) => {
