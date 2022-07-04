9abf9f85354be54524013fea9afd30b30fedb1aa
GWorsnop <georgeworsnop@gmail.com>
2022-07-04 11:49:33 +0100

    create controller to interact with API request

---
+++ app/controllers/categories-controller.js
@@ -0,0 +1,9 @@
+//controller here
+
+const { selectCategories } = require("../models/categories-model");
+
+exports.getCategories = (req, res, next) => {
+  selectCategories().then((categories) => {
+    res.status(200).send({ categories });
+  });
+};
