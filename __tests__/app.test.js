c9b864a68a163b45ef56c89e505488d1c9101520
GWorsnop <georgeworsnop@gmail.com>
2022-07-04 15:43:52 +0100

    Complete test of working endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -61,6 +61,25 @@ describe("GET: /api/reviews/:review_id", () => {
   });
 });
 
+describe("GET: /api/users", () => {
+  test("200: returns array of user objects with correct keys", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.users)).toBe(true);
+        expect(body.users.length).toBe(4);
+        body.users.forEach((user) => {
+          expect.objectContaining({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
 describe("Generic errors of API", () => {
   test("ERROR 404: returns bad path if wrong endpoint written", () => {
     return request(app)
