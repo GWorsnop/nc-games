fd187b70312c9d7f676f2310d63dc37a8e911af0
GWorsnop <georgeworsnop@gmail.com>
2022-07-04 12:23:31 +0100

    remove console logs

--- app/models/categories-model.js
+++ app/models/categories-model.js
@@ -2,7 +2,6 @@ const connection = require("../../db/connection");
 
 exports.selectCategories = () => {
   return connection.query("SELECT * FROM categories").then((result) => {
-    //console.log(result.rows);
     return result.rows;
   });
 };
