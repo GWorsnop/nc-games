42382b507370a9109dc7b7342bdc4eacf3246da5
GWorsnop <georgeworsnop@gmail.com>
2022-07-04 16:02:31 +0100

    Update index to include users functions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,5 @@
 const { getCategories } = require("./categories-controller");
 const { getReviewById } = require("./reviews-controller");
+const { getUsers } = require("./users-controller");
 
-module.exports = { getCategories, getReviewById };
+module.exports = { getCategories, getReviewById, getUsers };
