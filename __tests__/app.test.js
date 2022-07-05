3374ce9fe4a22a55df345fc1a206b37816126457
GWorsnop <georgeworsnop@gmail.com>
2022-07-05 10:23:57 +0100

    write tests for GET reveiws

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -149,6 +149,42 @@ describe("GET: /api/users", () => {
   });
 });
 
+describe("GET: /api/reviews", () => {
+  test("200: returns array of review objects with correct keys", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.reviews)).toBe(true);
+        body.reviews.forEach((review) => {
+          expect.objectContaining({
+            review_id: expect.any(Number),
+            title: expect.any(String),
+            category: expect.any(String),
+            designer: expect.any(String),
+            owner: expect.any(String),
+            review_body: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+  test("200: returns reviews ordered by date descending by default", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toBeSortedBy("date", {
+          descending: true,
+          coerce: true,
+        });
+      });
+  });
+});
+
 describe("Generic errors of API", () => {
   test("ERROR 404: returns bad path if wrong endpoint written", () => {
     return request(app)
