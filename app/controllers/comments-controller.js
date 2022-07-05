eda8db2307b721a6c4e4419a75dd95795bf4bb7a
GWorsnop <georgeworsnop@gmail.com>
2022-07-05 11:02:43 +0100

    create comments-controller and function

---
+++ app/controllers/comments-controller.js
@@ -0,0 +1,12 @@
+const { selectComments } = require("../models/comments-model");
+
+exports.getComments = (req, res, next) => {
+  const { review_id } = req.params;
+  selectComments(review_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
