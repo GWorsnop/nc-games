b95f257d26b69439a9daf2203a8013937c53983d
GWorsnop <georgeworsnop@gmail.com>
2022-07-05 13:16:42 +0100

    Add functionality for POST comment

--- app/controllers/comments-controller.js
+++ app/controllers/comments-controller.js
@@ -1,4 +1,4 @@
-const { selectComments } = require("../models/comments-model");
+const { selectComments, insertComment } = require("../models/comments-model");
 const { selectReviewById } = require("../models/reviews-model");
 
 exports.getComments = async (req, res, next) => {
@@ -7,6 +7,18 @@ exports.getComments = async (req, res, next) => {
     const comments = await selectComments(review_id);
     await selectReviewById(review_id);
     res.status(200).send({ comments });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.postComment = async (req, res, next) => {
+  try {
+    const { review_id } = req.params;
+    const newComment = req.body;
+    const comment = await insertComment(review_id, newComment);
+    await selectReviewById(review_id);
+    res.status(201).send({ comment });
   } catch (err) {
     next(err);
   }
