11dd3b45984f405f6b1b9bfae10db9db421a0009
GWorsnop <georgeworsnop@gmail.com>
2022-07-05 11:02:32 +0100

    add comments functionality to index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,12 @@
 const { getCategories } = require("./categories-controller");
 const { getReviewById, patchReviewVotes } = require("./reviews-controller");
 const { getUsers } = require("./users-controller");
+const { getComments } = require("./comments-controller");
 
-module.exports = { getCategories, getReviewById, patchReviewVotes, getUsers };
+module.exports = {
+  getCategories,
+  getReviewById,
+  patchReviewVotes,
+  getUsers,
+  getComments,
+};
