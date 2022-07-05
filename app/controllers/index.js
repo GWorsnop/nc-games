b6509366c765ea913ae05e8252c58edfe7c12ede
GWorsnop <georgeworsnop@gmail.com>
2022-07-05 10:23:32 +0100

    add to index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,15 @@
 const { getCategories } = require("./categories-controller");
-const { getReviewById, patchReviewVotes } = require("./reviews-controller");
+const {
+  getReviewById,
+  patchReviewVotes,
+  getReviews,
+} = require("./reviews-controller");
 const { getUsers } = require("./users-controller");
 
-module.exports = { getCategories, getReviewById, patchReviewVotes, getUsers };
+module.exports = {
+  getCategories,
+  getReviewById,
+  patchReviewVotes,
+  getUsers,
+  getReviews,
+};
