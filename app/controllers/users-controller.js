86fc754ea29d7fa0a6dde29f2ae7c2651acc31ea
GWorsnop <georgeworsnop@gmail.com>
2022-07-07 11:36:24 +0100

    Add functionality for GET users/:username

--- app/controllers/users-controller.js
+++ app/controllers/users-controller.js
@@ -1,7 +1,19 @@
-const { selectUsers } = require("../models/users-model");
+const { selectUsers, selectUserByName } = require("../models/users-model");
 
 exports.getUsers = (req, res, next) => {
   selectUsers().then((users) => {
     res.status(200).send({ users });
   });
+};
+
+exports.getUserByName = (req, res, next) => {
+  const { username } = req.params;
+  console.log(username);
+  selectUserByName(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
